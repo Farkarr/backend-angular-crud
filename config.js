8ae42181e6a6d9d4b5aabfb4d1eac2929e194938
karo <karoxx98@gmail.com>
2020-02-02 22:19:39 +0100

    he cambiado la uri de mongo para la nueva base de datos

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const DBCONFIG = {
     PORT: process.env.PORT || 3700,
-    DB_MONGO: "mongodb://farkar:farkar98@ds263368.mlab.com:63368/heroku_0kl19phl"
+    DB_MONGO: "mongodb://farkar:farkar98@ds215229.mlab.com:15229/heroku_bhfr8r2c"
 };
 
 module.exports = DBCONFIG;
